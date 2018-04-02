7216b2e6232bec14a2f580ce74228e66faf7809f
Fer <Fer@MSI>
2018-04-02 04:02:12 +0200

    newer version hotfix

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World second edition! hot fix!";
+		return "Hello World second edition! hot fix other change!";
 	}
 
 	public static void main(String[] args) throws Exception {
